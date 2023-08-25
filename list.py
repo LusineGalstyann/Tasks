8d278a3ab8d12f509f84d764363a7bd6233e39f1
LusineGalstyann <lusinegal8@gmail.com>
2023-08-25 23:40:54 +0400

    Task 2: Tail Swap

--- list.py
+++ list.py
@@ -13,17 +13,35 @@ def array_diff(a, b):
         if i not in b:
             rezult.append(i)
     return rezult
+
+
+
+
+
+
+
+
+
+
+
 #task 2
+# You'll be given a list of two strings, and each will contain exactly one colon (":") in the middle (but not at beginning or end). The length of the strings, before and after the colon, are random.
+#
+# Your job is to return a list of two strings (in the same order as the original list), but with the characters after each colon swapped.
+#
+# Examples
+# ["abc:123", "cde:456"]  -->  ["abc:456", "cde:123"]
+# ["a:12345", "777:xyz"]  -->  ["a:xyz", "777:12345"]
+
+
+
 def tail_swap(strings):
-    ind1=strings[0].index(":")
-    print(ind1)
-    ind2=strings[1].index(":")
-    print(ind1)
-    st1=strings[0][:ind1]
-    st2=strings[0][ind1:]
-    st3=strings[1][:ind2]
-    st4=strings[1][ind2:]
-    list([st1+st3],[st2+st4])
-    pass
-listss=(["as:bd","asdyy:123"])
-tail_swap(listss)
+    st1 = strings[0].split(':')
+    st2 = strings[1].split(':')
+    return (([st1[0] + ':' + st2[1]], [st2[0] + ':' + st1[1]]))
+
+#
+# list1=(['abc:123', 'cde:456'])
+# print(tail_swap(list1))
+# list2=(['a:12345', '777:xyz'])
+# print(tail_swap(list2))
