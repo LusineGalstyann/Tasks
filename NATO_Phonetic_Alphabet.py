6a6d67236f951b66976daf69c58e67e6a8388266
LusineGalstyann <lusinegal8@gmail.com>
2023-10-05 01:50:21 +0400

    Corrected a typos in NATO_Phonetic_Alphanet

--- NATO_Phonetic_Alphabet.py
+++ NATO_Phonetic_Alphabet.py
@@ -1,6 +1,6 @@
 def nato(word):
     str=''
-    dic=['Alpha', 'Bravo', 'Charlie','Delta','Echo','Foxtrot','Golf','Hotel','India','Juliett','Kilo','Lima','Mika','November','Oscar','Papa','Quebec','Romeo','Sierra','Tango','Uniform','Victor','Whishkey','Xray','Yankee','Zulu']
+    dic=['Alpha', 'Bravo', 'Charlie','Delta','Echo','Foxtrot','Golf','Hotel','India','Juliett','Kilo','Lima','Mike','November','Oscar','Papa','Quebec','Romeo','Sierra','Tango','Uniform','Victor','Whiskey','X-ray','Yankee','Zulu']
     for i in word.lower():
         str+=dic[ord(i)-97]+" "
     return(str.rstrip())
